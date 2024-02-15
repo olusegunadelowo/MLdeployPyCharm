1d63d2fa76d0d0d73506dc32318bee8f9a60f637
Olusegun <olusegunadelowo1@gmail.com>
2024-02-15 20:55:01 +0100

    flaskandhtml

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # Import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # Create an instance of the Flask class
 app = Flask(__name__)
 
 # Register a route
-@app.route('/')
-def home():           #decorator
-    return render_template("index.html")
+@app.route('/' , methods=['GET', 'POST'])
+def home():                #decorator
+    text = ""
+    if request.method == 'POST':
+        text = request.form.get('email-content')
+    return render_template("index.html" , text = text)
 
 # Run the Flask application
 if __name__ == '__main__':
